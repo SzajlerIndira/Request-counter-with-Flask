6dbec21932bba7a14f0f158d0649cf2ab2fb3d8b
Szajler Indira <szajler.indira@gmail.com>
2018-06-13 09:56:16 +0200

    index.html created, server.py/counting() added

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
 
+counts = 0
+
+@app.route('/request-counter')
+def counting():
+    counts += 1
+    return render_template('index.html')
 if __name__ == '__main_':
     app.run(
         host='0.0.0.0.',
